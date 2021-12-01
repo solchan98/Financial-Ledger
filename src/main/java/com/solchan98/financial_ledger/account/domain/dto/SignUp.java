2ac9d27dcd4d2ed9bdd730e11e9c1fc9791f85c5
solchan98 <dev.sol0427@gmail.com>
2021-12-02 01:33:30 +0900

    PAYHERE-#3 회원가입 DTO 작성 및 회원가입 인터페이스(유저 서비스) 작성

---
+++ src/main/java/com/solchan98/financial_ledger/account/domain/dto/SignUp.java
@@ -0,0 +1,28 @@
+package com.solchan98.financial_ledger.account.domain.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+public class SignUp {
+
+    @Getter
+    @Builder
+    public static class Request {
+        private String email;
+        private String name;
+        private String password;
+    }
+
+    @Getter
+    @Builder
+    public static class Response {
+        private Long id;
+        private String email;
+        private String name;
+        private String password;
+        private LocalDateTime loginAt;
+    }
+
+}
---
+++ src/main/java/com/solchan98/financial_ledger/account/service/AccountService.java
@@ -0,0 +1,7 @@
+package com.solchan98.financial_ledger.account.service;
+
+import com.solchan98.financial_ledger.account.domain.dto.SignUp;
+
+public interface AccountService {
+    SignUp.Response signUp();
+}
