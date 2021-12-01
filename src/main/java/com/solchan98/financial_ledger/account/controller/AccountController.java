38d235b5fe648bd8c24232eef9b5965ad0307603
solchan98 <dev.sol0427@gmail.com>
2021-12-02 06:21:43 +0900

    PAYHERE-#3 디렉토리 구조 수정(AccountController 위치 이동)

--- src/main/java/com/solchan98/financial_ledger/account/controller/AccountController.java
+++ src/main/java/com/solchan98/financial_ledger/account/controller/AccountController.java
@@ -1,4 +1,4 @@
-package com.solchan98.financial_ledger.account;
+package com.solchan98.financial_ledger.account.controller;
 
 import com.solchan98.financial_ledger.account.domain.dto.SignUp;
 import com.solchan98.financial_ledger.account.service.AccountService;
