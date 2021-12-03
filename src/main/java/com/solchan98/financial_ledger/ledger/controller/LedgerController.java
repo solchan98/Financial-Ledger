40302f2fa1cec0021d3d2e52d7fd5285f64d6845
solchan98 <dev.sol0427@gmail.com>
2021-12-03 18:05:29 +0900

    PAYHERE-#4 가계부 생성 Controller 작성 및 Ledger 예외 핸들러 작성

--- src/main/java/com/solchan98/financial_ledger/ledger/controller/LedgerController.java
+++ src/main/java/com/solchan98/financial_ledger/ledger/controller/LedgerController.java
@@ -1,9 +1,23 @@
 package com.solchan98.financial_ledger.ledger.controller;
 
-import lombok.NoArgsConstructor;
+import com.solchan98.financial_ledger.account.util.AccountUtil;
+import com.solchan98.financial_ledger.ledger.domain.dto.LedgerDto;
+import com.solchan98.financial_ledger.ledger.service.LedgerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@NoArgsConstructor
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/ledger")
 public class LedgerController {
+
+    private final LedgerService ledgerService;
+
+    @PostMapping("")
+    public LedgerDto.Response createLedger(@RequestBody LedgerDto.Request request) {
+        return ledgerService.createLedger(request, AccountUtil.getAccount());
+    }
 }
