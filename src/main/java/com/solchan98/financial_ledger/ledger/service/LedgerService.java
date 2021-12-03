889c7479b82c5a6bc82f27c6faaf83e445639443
solchan98 <dev.sol0427@gmail.com>
2021-12-03 17:21:46 +0900

    PAYHERE-#4 가계부 생성 기능 구현 및 예외 케이스 테스트 작성

--- src/main/java/com/solchan98/financial_ledger/ledger/service/LedgerService.java
+++ src/main/java/com/solchan98/financial_ledger/ledger/service/LedgerService.java
@@ -1,9 +1,16 @@
 package com.solchan98.financial_ledger.ledger.service;
 
+import com.solchan98.financial_ledger.account.domain.Account;
+import com.solchan98.financial_ledger.config.Status;
+import com.solchan98.financial_ledger.config.content.LedgerContent;
+import com.solchan98.financial_ledger.config.exception.Message;
+import com.solchan98.financial_ledger.config.exception.ledger.BadRequestLedgerException;
+import com.solchan98.financial_ledger.ledger.domain.Ledger;
 import com.solchan98.financial_ledger.ledger.domain.LedgerRepository;
 import com.solchan98.financial_ledger.ledger.domain.dto.LedgerDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,8 +18,22 @@ public class LedgerService {
 
     private final LedgerRepository ledgerRepository;
 
-    public LedgerDto.Response createLedger(LedgerDto.Request request) {
+    @Transactional
+    public LedgerDto.Response createLedger(LedgerDto.Request request, Account account) {
+        checkLedgerData(request.getContent(), request.getPrice());
+        Ledger ledger = makeLedger(account, request.getContent(), request.getPrice());
+        Ledger savedLedger = ledgerRepository.save(ledger);
+        Message message = Message.builder().msg(LedgerContent.CREATE_LEDGER_OK).status(Status.CREATE_LEDGER_OK).build();
+        return LedgerDto.Response.getLedgerResponse(savedLedger, message);
+    }
 
-        return null;
+    private Ledger makeLedger(Account account, String content, Long price) {
+        return Ledger.builder().account(account).content(content).price(price).build();
+    }
+
+    private void checkLedgerData(String content, Long price) {
+        if(content.isEmpty() || price < 0L) {
+            throw new BadRequestLedgerException();
+        }
     }
 }
