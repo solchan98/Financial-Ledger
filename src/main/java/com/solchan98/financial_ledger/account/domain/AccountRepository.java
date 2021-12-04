9c56768c5eeac6a476388cddce1f5659682e29bc
solchan98 <dev.sol0427@gmail.com>
2021-12-05 02:05:24 +0900

    Revert "PLAYGROUND - N+1 문제 해결(단일 관계)"
    This reverts commit cb81897cf652cf07feb6883bd99521eda76fbf52.

--- src/main/java/com/solchan98/financial_ledger/account/domain/AccountRepository.java
+++ src/main/java/com/solchan98/financial_ledger/account/domain/AccountRepository.java
@@ -1,19 +1,13 @@
 package com.solchan98.financial_ledger.account.domain;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    @EntityGraph(attributePaths = {"ledgerList"})
-    @Query("SELECT DISTINCT a FROM Account a")
-    List<Account> findAll();
-
     boolean existsByEmail(String email);
+
     Optional<Account> findByEmail(String email);
 }
